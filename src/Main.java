a12bb041fc5e496ce0d0cee08dd2e92bce402f75
gada daniel <gadadaniel18@gmail.com>
2024-12-12 15:43:45 +0100

    inisialisation du joueur 1 et 2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,51 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+import java.util.Scanner;
+public class Main{
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        char[][] plateau = {
+        };
+        char joueurActuel = 'X';
+        Scanner scanner = new Scanner(System.in);
+        String continuer;
+        do {
+            initialiserPlateau(plateau);
+            jouer(plateau, joueurActuel, scanner);
+            System.out.print("Voulez-vous jouer une autre partie ? (oui/non) : ");
+            continuer = scanner.next();
+            joueurActuel = 'X';
+        } while (continuer.equalsIgnoreCase("oui"));
+    }
+    public static void initialiserPlateau(char[][] plateau) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                plateau[i][j] = ' ';
+            }
         }
+    }public static void afficherplateau(char[][] plateau) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            System.out.print(plateau[i][j] + " ");
+            if (plateau[i][j] == ' ') {}
+        }
+      
+    }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
